11948d425696a70c4cfaede21fcf05de5cd8fb95
RyuzakiKai <129240607+RyuzakiKai@users.noreply.github.com>
2023-03-29 23:36:20 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,126 @@
+<script setup>
+
+</script>
+  
+
+<template>
+    <h1><center>CHICKEN KING</center></h1>
+    <center><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAA1VBMVEX////+/v4AAADtGyQaGhr8//8PDw8YGBgVFRXi4uLqAABXV1czMzPU1NSJiYnsAAChoaHtABD19fWzs7Pc3NztFR9dXV0ICAjFxcUnJyfs7OwRERFsbGzlAAAfHx8rKytFRUXtDRmBgYGTk5O9vb14eHj1zs46OjpNTU2cnJzrRkztkpP45eTz09TtJC357ezvpKbqb3HzwMHtXmTtfH7xra7pVVjqLjb56enwub
+        jrZWvwmp3oPkbqd3vtiI3rTFTrio/rOD3vur7wr7HuISvtV17tanDPEG1dAAARVElEQVR4nO1dC1uqzBaGwdGk3AiFlqlFpbVVwFuSmp
+        pp/f+fdNYMqCjQlzNasJ/znu/slJvrZa1ZtxlUEGIKUST/MZyVGABBhnMEhrN+CSKbrInhJzCZKLXsw8tyFDCbW3LGoSgmR1Ym/NvsG
+        MdgYkCdRVIIMsuZDIKMjjsZ5FwwMExQOKPenuEkttN+B0zxLElOlFXWRBFMjLAMSJTDYEGSRhML/m12FEmhyCFmQiiKLF0E14EmgyCTu
+        0gQPTepZGCYnDDBXBckZAyyC5oUfglSBStiz0/kSgwT0cfjEDERHp+x478+O/YEOTUYe34cpYt3YtwpcnmJBFgoU77sOzkBBHmLnnhT5F
+        JgMpAAK3Pxj3caBbaglCwDZfQXiWLIcs6/TjA5FR37KosDC3JUJErYPZEgY2OBmDB/sT+SQ481d0qMibI1chKQ2PvAtvAzMWBr+JMzMPw
+        nilrshyPr0tZWvT3tDBqv89fGoDNt11ux5bm3YFjAdrc/s1TVMAydAP6qit7od218DAn5wOAvcHckqaouUWoqASEqSbqhSqNuzDiKe/tRb
+        I+XikHoVAzrtddvjtvjZr/3ahmqASwNeTnWDkmSswuwf0DTxhLRnqrOJ0NT8+8wh5M51ayqN7XI8xlk5OC4txPF+NlRQX2K82aGkdDMt7k
+        CilSdZ3xAPfL1U/dCa0B0JM/eRSGcAMbi+0wmehy0OMTaAs+i1n01+FyB8ac06l+rB9cbChmPzwfRInOs9cjtc7Y2UYj9vQtb+gveJdg7JLasTA4wGtmTif1n6
+        O0ZSK32dwLeNFSj2O6Tg2c2o3QrcDX89x6CjiEZend7IzYhbuzQ8K78DgHScFp8lsqTDu67mrJlGUTgna3YlCV5d+NC7xFe5JboFru/2d9
+        PhF3i2yAE1Y+QcdVT56utXnwQXw2lTV5oH6pkEIqMteeP5vP2KxD8DAtxuCO9uK9eTG/LwFDf6Av8CRRfWcfij06c4Z4qqaPQz8NNpeMSNyfelrose2TFEfgbMNl9JWV7FJMdojCVJaMR8ZFt1eOzkD1ttedrtYkNGItvTKvNfpIh7srgMqKMbSjrr0SJ+E15o8rU/DdDcyxH/Y8MIQQ/q0FsLyFDMUN20D91WVIXAh1+FToip8rMJ5+pW9LS3leLP6tDDMFbbge3m24gaFUkyQEF2+BVRiTWy8aGIbx41iFN2FeJP9sWqYPovZDttkPTmxdIVdUx5KwkiRlioW/oPh1Chj4CN1z/MWkZANYnGaFx26KBwLYkKKdeMPkj6a0WuJYVQ3eSHrYYg5jV/X4QN6M2NwJi+90bgBDsSY6jzUlh32nJhOHS/txiSPtxTXWpxK214QNuQLHnGxSaIw+8t1PD0BcCngFDSTanxErbJqmPXYbroSTCFRrxZQiu0hj73puqvornbWBTGbyMCEP9VXN0fU5M2qdDYX2gEt+R+Lm0rK1ujAOlrfuS+BZJrk8MYp9K24RUZkhslTDcKj41SSduNp6wDUtq+i0Mm7OOPKMx/FldEoZ9yhAiZrdr05d+X+qeAxas8paKx8IzMNxxpFgYK3IPLHWsEB12py5Dfdk1HX3D0M+yBQxDImocgHuS9RHYKM4MXR6Z9tsSBuWKoaSrsu6+GARSkoEe14Dx4ljSOLjZNKB+Vzstuz2vmBOP4RqU4fYJYxiyLz8j8p7ogpEGM1LwjsRADfXTFLA2Bz8iK3IFAnulIiuqoQa9CgQRuRtynd/Hm6TPQzPEAVWcoSw7c0O2+qvut9iqL6bONFAqi3NdnR5bWCb0wM2Hjp8X3bNIQ268b98DrLVn4x3PCclpaG77m6BS23OSVIcBLyouRXUcvAPY7hhtcWs7DMR5zOIFzSmhblDDhw/Gn9RO5feQ5g2GbFadbQWZLhQoR3I1HKsswEFA1h3maAhsCwx1ld4EgO1BZejjbqrLyCvxgfHLp7zFye9w5yNtC4oOd5Dil1ZImxG/Vaabz7aB4fvekhwLm04XpMx69HF92TCBodmRK1Y/5Ea05dGGuX6krIbxsUoPGPyDFX0c/hwJuP5pyIaqq1Y9ON+2kBtr4laUz+IE08yGuH6O7GuGrfZg5ECA7y+elxA2+i87FEWg6KwoHoch8xNv3mlf6BCC3uusOZ5DNrZ8VmZvkqUbb4HRiNvyauLC0sPiCjc414p/wXDRGLfqbdrU1hfzaaczAyohDN5kxw0SR2HIvRYekyo+ZDNuDZqa1p62IPC96lKjOXldGBGZwafiRnopxNOIbM+TrOqWQzyGvYBoEQwE2HRMut6L/AOln646HUsZhl9Cm6szTBrhkrwI6pBtebj79yDftlJXJDXYSLStFra7z+32sFvHmJaHUEx1ImzQhDILkxsRbNUwfqfP5vS9Tw6gJYc1c6fjRe9zYbrX9+p6yXA00Q6dt2+qSpO0lQMzqcyPaIiHa/vb4OPbu7rB1rIx6vR6g8HHaDI1vLp+aQ9fnbkZokjN0eUh2Htgbodj4vRwU4sDPWh9YsVpdlsAs/vcU726XjafZWPSnITNoS5k3Wro+iAgKo9kh2LYN9yps61rf66XC+GG18FQm7TnPXwLY4hBxTBQ+weSicpwsCsN5Yg5CwrclHtud83RZoakf8zC+75j2ljdcrbcU9iH4tjS3cnBiL2VfkN3y2DwJFBpzCOOo31i352K0UOHGMZPVLdaxH156PUyXkagQseKaN2L88DMRXwYNiGgR9XmdqWzUN0WMG3bGJOoy/QN3/zVAdbJHBKkWx1VE4xlc7JcEmpjMoVhvEYtYyP3aZU5uAMwPgQFAcw0vJ9IZ0gbriftjr5c/gRWqs9Wr2OlQIKhIkWknKbat91haNRhnBl1HCX51jXi9uQCxhaZFgwLc0O5a9KJX2m5+KqljTGo0FotCYudDkUoYqEqCNv15k0XSpLVNCDaRVZ/C7hCIPeLEcgg2l1gSXQBscJsu67UGinNaAb2MnooxwNDcIS7GRcYG57K5thluLQaYjRDEiqis4Y4gMzN73a+YSzhZ7nb9hLviMY4Bel292JsowQmyVdsvF16itis9IYuQ30WyQDbUD9+kdoeG98bHbitSMbHhsRqDntudNziUB5GM/wwJOUX3cx3x3/PkOTJqj+yPs+UV/PbkcmMMJHDl4z9DL6fPWkOUKTu0hfMRPfxCyAYmOlfAcoryXB+bVZtnxLNlHS6PG8nJTEHsqx6q7pDQPqteugs+U9hjyBVV3VJnuLtskDEuPXc7IwiHAnEE0n/vWWJexbZZAGfJI/E76/Zxngkkzjym4FiH4ai0DXAUBth6hJDC8hWA0zU6DInoZwJOkOJhuuSAf8LeerODBoixs/k6OX+K7zXEnISZPo22tYrWWD5EVBjN8iw1YNxq74yP00i8PUBmGtscaKQtVDTHfff3g74WLCnKqRByoRDDVw9bZ6ThxZRo0pnnVbbcHNruQJuTVVykBUxVfNNcH2LG8fZ5Kk7snK98rl5LB33m7793c8K6dkEnuLbT0QeBXJ/BZzZI8maITsjd3IG476XeIvm4tNxd/b4wjzHxNkh2ly43pNV8vC9alizfntY/9Tr3WG7P3MMol9dlXvsLpQKyfdttDwfvUar6aiUpG6osgLVgwLJG/3KAVV1mpy1Et9TeQfrAon1aUNXyFcprBaV6uRrI2Zvdc4PoKULzzUO2S9p1ceTgQNuByA5g8m4e4BKN2adOBBIs11o24KxqyFmBCPB4rJFfkf/g2ARNWE/ZcP2y0lHEOQ4YPy5ymTpkAG/+tVoh/hkUStlS1oMlFQqADbtwYvTQuG0tpHr4qlQuPMfnz8tnJ773pMTysHL5u4eEcHj3eVvk8yidLG6ZpitFtPoqrTZnUNpdOo//g8cj3L+A6onN7sXvSggdJIiOEHo9vIYcn8f2XTm5GrFsHSWTqVvfASBQCq9wzCVuj5bnSDCAVeZs51r5lERyF0Xi2lCM41qv6pGP0PtsZhK+zUYwTCFCuv3Fyh1ssPwHmVSGdBd7a5wheB18ezXGd5o1Le4BLNbu8MZptA9eS26B+wwfEDEOP+4N+qihorVrXv24/AYEt99i1LXqW2CZJhdhzBMoUvP4QYYlq6JBjdjL/eYE34Vax2KBZC1eLGzO5RhOpM6SRHFiCEM7+AA9ODbcMhvOmMBMKxSx1EGUVHgdodZabpWPYGtYqiVlmAfqh1Z6L2QTVcpQ0IwHbSnMIbo/BJlqsU/oTqEUZhBO6b+u8heX10BQ7CtTCokcIUyzFNbRefeAVsM73ZP+HVkUzdXNyIdXSGpSRRD4bQIqroQggwL6RRaJUHna/CMRd7sNVutps68EPAQ3B3FUIOc5fomaKXi03UK/fXeoDW4zJarUeUyTBGFnKROToKCRDGEHZkUKn/NsOpmbqlM+rcYEgPInmRI+pjOX4F/vA1cLJIhpGYpwuUrKz0h6itmOBm6Vso8a0eiRYpGaJKKoLvdA6IZEu+bQbndrM3vaS4oHk84GIqrHhALQ/dMypCOwI1/9OELhtrjderk5mGH4TnaUZl4e83H0Ps/Sx/IHYfA0HMxt+lgKPuCIRReGfA2mZ2IT673x/9BnAxFtxnA0iZxuySrvJQIdw1D8WzbsX/FkOgLouhO1lZDNGvdfA63DgW2iLG6K/7q6ZL4x+2M60uGNAfdZZhF25l36Yx5HHJ2crxG11YFfE/uf95/1NcMxadigCF1slA9uaRK+Zs0RCMWhr5OHBfRLYabVGWFrxkKJSgyAhVwjRbJ6dtyrfyUQqlqtXjDktMcqhe3zVBLQ+DP+C78HwzBsIMMwRRAb5nrYrEIteJVtVjeu+HvOvrDtAayqIg2nShgVCz6ehTk/W4nqrjOWQj+wvtAJyr3hFAaUonMSRqhwv4V8EG7HqVyuVzzGVEe3hc2fuKiUC77fT/dv5W/1uD84GVzd09XRXR9Vrjfram/g9itfw9HCUBvHcuURlJ+KoSAcS3PgaUIg3Z5STWQva+FesKL+2/0KuKsjL/o6YzG7XPkpTna6amfah5V/+sasZ4BvQeHeU6d5PnKtT5sF7J//S43CrHlR8Ig4fAokPDglVRifquPU3O7wpGItQJJ7UdolYjObsFWt5rWmvvuFiqSkrfa3dsP7/xHxpifUKquC0UxVa0hMiA1RM1SK6Pbapa+zZ/RXEH8c3VFOsl/YIvX7o/bY5VBZNG6OMgVz86faJ5WpmVHGV2eExo5lL7XaDlZQ9lzUhFCdnRfQ27DLva/DZXbMMyDzLf03SnRaxZda9m7HN1e+uMSOyO0SYr66A3OOMcID6DDVWZWRvlSGpEwUSRFxwN6dGUvoPwTnQSALadFcnQedHxD70UCViBoN2TMZYlVPhZzlzRq5BApIR6QNxmI0EWNGu8DunFTT+BcQmQm7TBfQ3Jk5BF6KlOrrKJSHp3dUyb3mlACd/JQpn9LsOVvibw6v4QtwDmXQ9U76k3jzg9wfvtYI6rJFmpC7pEUQNlHWnKc354REhend5pQfswDlfPHsxqh9VQuabenIX3zmMJvZqLv37Ad4s6GpCD+Y4kT/zg9ITkqZO0Wxz9mr8DEMCHcKBgJxjtr9oP967USwpBZFUnxMkkwNS4R48+P80eskvD7yxytnJ/9iS9mcPnsZDh8HoLxZ8guYjIMlPPH1hLAkavfmAAL5Y2DcWfI3U1Nho0mAYx3UmQ/9afBaGtxb/hvgW2G/giCeJc++BUZf6bgH1agkJiEmSBRymAC8w8x/B/xQLwXIRwASWgfcOKf5he/782NBtNgSgg3F8z98EMLcjwwJiWHFuNo4PnJkEQg/vkhn3zxT7k4BYw5OwLOxZvxZ8j7HWexB+8TT/HnyNEMT8QUNo8fTID6OAnGP0rwmljsKztx/Q/rBWJOMAm5yBocviIJ4FmCcFBBjgcWY0uOfRKwLYmMOOl/Fy5TBx9gWx8AAAAASUVORK5CYII="
+         alt="Logo Restoran" style="width:120px;height:120px;"/></center>
+    <h2><center>Masukkan Data Pelanggan</center></h2>
+ <form action="/action_page.php"><center>
+        <label for="name">Nama : </label>
+        <input type="text" id="name" name="name"><br><br>
+        <label for="HP">No. HP : </label>
+        <input type="text" id="HP" name="HP"><br><br>
+        <input type="submit" value="Submit"></center>
+        <p>Hanya di Chicken King anda dapat menikmati ayam goreng tanpa takut kantong jebol</p>
+        <strong>Cukup Dengan 12k perut kenyang, hati senang, dompet aman.</strong>
+        <h3>Menu : </h3> 
+    <table >
+        
+        <tr>
+            <th>Makanan</th>
+            <th>Harga</th>
+        </tr>
+        <tr>
+            <td>Dada</td>
+            <td>10k</td>
+        </tr>
+        <tr>
+            <td>paha</td>
+            <td>9k</td>
+        </tr>
+        <tr>
+            <td>sayap</td>
+            <td>8k</td>
+        </tr>
+    </table>
+    <br>
+    <table >
+        <tr>
+            <th>Minuman</th>
+            <th>harga</th>
+        </tr>
+        <tr>
+            <td>air mineral</td>
+            <td>6k</td>
+        </tr>
+        <tr>
+            <td>Sprite</td>
+            <td>8k</td>
+        </tr>
+        <tr>
+            <td>Fanta</td>
+            <td>8k</td>
+        </tr>
+        <tr>
+            <td>Coca Cola</td>
+            <td>8k</td>
+        </tr>
+    </table>
+    <br>
+    <h4>Pesanan</h4>
+    <form action="/action_page.php">
+        <label for="makan">Makan :</label>
+        <select name="makan" id="makan">
+            <option value="pilih">Pilih</option>
+            <option value="dada">Dada</option>
+            <option value="paha">Paha</option>
+            <option value="sayap">Sayap</option>
+        </select>
+    </form>
+    <br>
+    <form action="/action_page.php">
+        <label for="minum">Minum :</label>
+        <select name="minum" id="minum">
+            <option value="pilih">Pilih</option>
+            <option value="air">Air Mineral</option>
+            <option value="sprite">Sprite</option>
+            <option value="fanta">Fanta</option>
+            <option value="coke">Coca Cola</option>
+        </select>
+    </form>
+    <br>
+ <form action="action"><label for="makan dimana">Makan dimana? :</label>
+  <select name="makan dimana" id="makan dimana">
+    <option value="dinein">Dine In</option>
+    <option value="takeaway">Take Away</option>
+  </select>
+  <br><br>
+  <input type="submit" value="Submit"></form>
+ </form>
+ <form action="/action_page.php">
+        <p><label for="resto">Catatan untuk Chef :</label></p>
+        <textarea id="saran" name="saran" rows="4" cols="50">--ketik disini--</textarea>
+        <br>
+ </form>
+    
+
+<h6>Untuk Menu Lebih Mahal silahkan kunjungi 
+    <a href="https://kfcku.com/food">KFC Original</a>
+</h6>
+<br><br>
+<h5>Copyright © 2023 Arfan Muhtada.</h5>
+</template>
+
+<style scoped>
+  
+
+</style>
+<style>
+body {
+    background-image: url("bg ayam baru.jpg");
+  background-size: cover;}
+  table, th, td {border:2px solid #000000; } 
+</style>
